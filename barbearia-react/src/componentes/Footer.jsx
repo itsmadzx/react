fdd86587f8e998b296615097f833e9312e6cbe93
Aluno <e023aluno@outlook.com>
2026-09-09 16:15:43 -0300

    adicionamos o código para o footer e o header

---
+++ barbearia-react/src/componentes/Footer.jsx
@@ -0,0 +1,19 @@
+function Footer() {
+  return (
+    <footer className="footer">
+
+      <h2>Barbixas</h2>
+      <p>Seu estilo, nosso cuidado.</p>
+
+      <div className="footer-info">
+        <span>📍 São Paulo - SP</span>
+        <span>📞 (11) 99999-9999</span>
+      </div>
+
+      <small>© 2026 Barbixas - Todos os direitos reservados.</small>
+
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ barbearia-react/src/componentes/Header.jsx
@@ -0,0 +1,20 @@
+function Header() {
+  return (
+    <header className="header">
+
+      <div className="header-logo">
+        <h1>Barbixas</h1>
+      </div>
+
+      <nav className="menu">
+        <a href="#">Início</a>
+        <a href="#">Serviços</a>
+        <a href="#">Agendamento</a>
+        <a href="#">Contato</a>
+      </nav>
+
+    </header>
+  );
+}
+
+export default Header;
